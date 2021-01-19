3c8362105c67fd636e636be21c971ea6b183b3a6
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-19 16:49:44 -0500

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def h_HBNB():
+    """return hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def h_HBNB_():
+    """display hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """display c followed by value"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def text_py(text='is cool'):
+    """display “Python ” followed by value"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
