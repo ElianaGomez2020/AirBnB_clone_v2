55a2d411e7bef682a226171beee2d34c9392f8a5
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-19 16:07:56 -0500

    Web framework

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,15 +3,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def h_HBNB():
     """return hello hbnb"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def h_HBNB():
     """display hbnb"""
     return 'HBNB'
-    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port="5000")
