98eafab906ea96badeb4170337203d2c84d75076
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 18:17:25 -0500

    tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+""" Start Flask Web Application """
 from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """method to handle"""
+def teardown_ses(self):
+    """ close storage """
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
-def state_l():
-    """display a HTML page"""
-    state = storage.all(State).values()
-    return render_template('7-states_list.html', states=state)
+def states_list_route():
+    """ varible sends a numeric in HTML """
+    storage.close()
+    return (render_template("7-states_list.html",
+                            states=storage.all("State").values()))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host='0.0.0.0', port="5000")
