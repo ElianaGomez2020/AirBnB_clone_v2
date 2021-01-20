e5645c021a5fabe43a06888cfc9cb0e8fe4ddcf9
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 17:22:36 -0500

    list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from Flask import Flask
-from Flask import render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def closes():
+def closes(self):
     """closes session"""
     storage.close()
 
