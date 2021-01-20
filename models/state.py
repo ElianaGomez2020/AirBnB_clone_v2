dc5168c0af3b3401bb8b7abf97713cdcaef68f7b
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 10:11:49 -0500

    engin

--- models/state.py
+++ models/state.py
@@ -3,9 +3,9 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.city import City
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
 from os import getenv
+from sqlalchemy.orm import relationship
 import models
 
 
@@ -14,13 +14,14 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="states",
+        cities = relationship("City",
+                              backref="states",
                               cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
             """returns the list of City instances with state_id"""
-            all_cities = storage.all(City)
+            all_cities = models.storage.all(City)
             cities = []
             for City in all_cities.values():
                 if city.state_id == self.id:
