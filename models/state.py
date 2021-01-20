a251910da2b722efa553002260e9e3e8eb659fa9
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 10:03:42 -0500

    engine

--- models/state.py
+++ models/state.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
     """This is the State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states",
-                          cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """returns the list of City instances with state_id"""
-        from models import storage
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="states",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id"""
             all_cities = storage.all(City)
             cities = []
-            for value in all_cities.items():
-                for val in storage.all(State).items():
-                    if value.state_id == State.id:
-                        cities.append(value)
+            for City in all_cities.values():
+                if city.state_id == self.id:
+                    cities.append(city)
             return cities
