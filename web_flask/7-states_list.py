4f79141d2f38697a2be1b7b6d492a8bc48cadf32
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 17:37:42 -0500

    modisy

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def closes(self):
 def state_l():
     """"return list states"""
     list_st = storage.all(State).values()
-    return render_template('7-states_list.html', states= list_st)
+    return render_template('7-states_list.html', states=list_st)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
