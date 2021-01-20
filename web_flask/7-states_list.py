f8a4d90c6381d4e6db988a5360dc8bdc9604f137
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 17:04:19 -0500

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,9 +16,8 @@ def closes():
 @app.route('/states_list', strict_slashes=False)
 def state_l():
     """"return list states"""
-    storage.close()
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+    list_st = storage.all(State).values()
+    return render_template('7-states_list.html', states= list_st)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
