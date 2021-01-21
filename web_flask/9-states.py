83a31a37bb9b542f4f89d041da49541e93f0e286
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-20 23:11:57 -0500

    Cities by states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-"""Start a flask app"""
+"""Start Flask WEB"""
+
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states', defaults={'id': None}, strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    """Display Html page with a list of all states"""
-    states = storage.all(State)
+def states_id(id=None):
+    """ State with id  """
+    states = storage.all('State')
     if id:
-        id = 'State.' + id
-    return render_template("9-states.html", states=states, id=id)
+        key = 'State.' + id
+        if key in states:
+            states = states[key]
+        else:
+            states = None
+    else:
+        states = states.values()
+    return render_template('9-states.html',
+                           states=states,
+                           id=id)
 
 
 @app.teardown_appcontext
-def close_(exception):
-    """close the current SQLAlchemy session"""
+def teardown(self):
+    """Removes SQLAlchemy session"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
